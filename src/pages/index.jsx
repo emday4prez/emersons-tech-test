48f53008fbf26f2393fc0e84302541406aedbf87
Emerson <emerson.day@gmail.com>
2023-03-20 11:39:17 -0700

    setup tailwind

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,7 +11,9 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className='min-h-screen'>
-        hello
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       </main>
     </>
   )
