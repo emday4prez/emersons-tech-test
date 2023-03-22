6a35e052e1d7db91288955e64e2304993b4cee58
Emerson <emerson.day@gmail.com>
2023-03-21 23:19:07 -0700

    removed react-charts

--- src/components/dashboard/GrowthChart/index.jsx
+++ src/components/dashboard/GrowthChart/index.jsx
@@ -1,9 +1,7 @@
-import RevenueGrowth from './RevenueGrowth'
-
 export default function Charts() {
   return (
     <div className="grid grid-cols-1 gap-4 pt-6 sm:grid-cols-2">
-      <RevenueGrowth />
+      revenue chart
     </div>
   )
 }
