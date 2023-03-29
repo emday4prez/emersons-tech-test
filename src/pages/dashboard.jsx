b437a569b0ac1e622c663c1adc8d014437cd9bc6
Emerson <emerson.day@gmail.com>
2023-03-29 08:14:17 -0700

    added profile menu test

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -233,7 +233,10 @@ export default function Sales() {
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    <Menu.Button
+                      data-testid="profile-menu-button"
+                      className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
